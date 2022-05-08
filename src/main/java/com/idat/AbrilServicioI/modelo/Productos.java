2fcb467b6f60b3221f9efc5e6d4daf522768e035
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-05-07 21:43:14 -0500

    initial proyect

--- src/main/java/com/idat/AbrilServicioI/modelo/Productos.java
+++ src/main/java/com/idat/AbrilServicioI/modelo/Productos.java
@@ -1,11 +1,19 @@
 package com.idat.AbrilServicioI.modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Table(name = "Productos")
@@ -22,6 +30,27 @@ public class Productos implements Serializable{
 	private Double precio;
 	private Integer stock;
 	
+	@OneToOne(mappedBy = "producto")
+	private Proveedor proveedor;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+	@JoinTable(
+		name="producto_cliente",
+		joinColumns =
+			@JoinColumn(
+					name="id_cliente", 
+					nullable = false, 
+					unique = true, 
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_cliente) references clientes (id_cliente)")),
+			
+		inverseJoinColumns = 
+			@JoinColumn(
+					name="id_producto", 
+					nullable = false, 
+					unique = true, 
+					foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(id_producto) references Productos (id_producto)"))
+	)
+	private List<Cliente> cliente = new ArrayList<Cliente>();
 	
 	public Productos() {
 		super();
