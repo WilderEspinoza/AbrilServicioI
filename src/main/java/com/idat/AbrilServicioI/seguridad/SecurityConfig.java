2a792d78da47da65a835dc0e08173a9ca240e266
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-06-09 22:37:47 -0500

    clase de JWT

--- src/main/java/com/idat/AbrilServicioI/seguridad/SecurityConfig.java
+++ src/main/java/com/idat/AbrilServicioI/seguridad/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.idat.AbrilServicioI.seguridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //	private PasswordEncoder encriptado;
 	@Autowired
 	private UserDetailService service;
+	
+	@Autowired
+	private TokenFilter filter;
+	
+	@Autowired
+	private EntryPoint entrypoint;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,11 +38,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/producto/v1/*").access("hasRole('USER')")
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.csrf().disable();
+		
 		http.authorizeRequests()
-			.antMatchers("/producto/v1/*").access("hasRole('USER')")
+			.antMatchers("/crearToken").permitAll()
+			.anyRequest()
+			.authenticated()
 			.and()
-			.httpBasic()
+			.exceptionHandling()
+			.authenticationEntryPoint(entrypoint)
 			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
 			.csrf().disable();
 	}
 
@@ -42,5 +65,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 
-	
+
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+
 }
